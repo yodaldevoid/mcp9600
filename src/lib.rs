91d124bc19ac87ae981b28c41ede45af7c2f9168
Gabriel Smith <ga29smith@gmail.com>
2024-10-13 14:29:26 -0400

    Move temperature types to main file. NFC

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 #![deny(unsafe_code)]
 #![no_std]
 
-mod types;
-
 use embedded_hal::i2c;
-
-pub use types::*;
 
 #[derive(Debug)]
 pub struct MCP9600<I2C> {
@@ -142,6 +138,18 @@ enum Register {
     DeviceID = 0b0010_0000, // Should contain the device ID
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Temperature(pub f32);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RawTemperature(pub u16);
+
+impl From<RawTemperature> for Temperature {
+    fn from(raw: RawTemperature) -> Self {
+        Self((raw.0 as i16) as f32 / 16.0)
+    }
+}
+
 #[derive(Clone, Copy)]
 pub enum ThermocoupleType {
     // Rather than mess around with constructing a bit vector, lets just make this a logical
@@ -225,6 +233,31 @@ pub enum DeviceAddr {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_positive_convert_temperature() {
+        assert_eq!(
+            Temperature::from(RawTemperature(0b0000_1100_0101_0010u16)).0,
+            197.125
+        );
+    }
+
+    #[test]
+    fn test_negative_convert_temperature() {
+        assert_eq!(
+            Temperature::from(RawTemperature(0b1111_0011_1010_1101u16)).0,
+            -197.1875
+        );
+    }
+
+    #[test]
+    fn test_from_raw() {
+        assert_eq!(
+            Temperature::from(RawTemperature(0b1000_1011_1010_1110u16)).0,
+            -1861.125
+        );
+    }
+
     #[test]
     fn test_sensor_configuration() {
         let config_byte =
